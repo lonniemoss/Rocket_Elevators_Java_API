5e122fe30ba3d7ce8b7f343806d69953530924dc
glory jacques <101422810+lonniemoss@users.noreply.github.com>
2022-12-21 13:06:02 -0500

    second commit

---
+++ src/main/java/com/glory/app/rest/RestApiRocketElevatorApplication.java
@@ -0,0 +1,13 @@
+package com.glory.app.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiRocketElevatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiRocketElevatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glory/app/rest/RestApiRocketElevatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glory.app.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiRocketElevatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
